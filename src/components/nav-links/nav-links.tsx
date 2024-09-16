9cd51c98d8da17c04386ceecd47030e0836b2664
MohammadHelaly <mohammad.helaly@gmail.com>
2024-09-16 08:37:14 +0300

    Small updates

--- src/components/nav-links/nav-links.tsx
+++ src/components/nav-links/nav-links.tsx
@@ -12,7 +12,7 @@ const NavLinks = () => {
       <li key={link}>
         <button
           onClick={handleClick}
-          className="flex h-full cursor-pointer items-center px-4 py-1 font-manrope text-base font-normal text-black transition-all duration-200 ease-in-out hover:bg-black hover:text-white"
+          className="flex h-full w-full cursor-pointer items-center px-4 py-1 font-manrope text-base font-normal text-black transition-all duration-200 ease-in-out hover:bg-black hover:text-white"
         >
           {link}
         </button>
